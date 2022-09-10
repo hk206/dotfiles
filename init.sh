f4a77e41d4e75ddfc2535ed81da55a3538c25759
Hokuo <48110124+hk206@users.noreply.github.com>
2022-09-10 23:59:17 +0900

    update: add mkdir's  p option

--- init.sh
+++ init.sh
@@ -11,9 +11,9 @@ cd $THIS_DIR
 
 # n
 ln -snfv "$(pwd)/.node-version" "$HOME/.node-version"
-sudo mkdir /usr/local/n
+sudo mkdir -p /usr/local/n
 sudo chown -R "$(whoami)" "/usr/local/n"
-sudo mkdir /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
+sudo mkdir -p /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
 sudo chown -R "$(whoami)" "/usr/local/bin" "/usr/local/lib" "/usr/local/include" "/usr/local/share"
 n auto
 
